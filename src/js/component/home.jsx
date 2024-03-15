1409231e131114797e7aedf4855d841ce1aa81f5
Carmelico92 <130740279+Carmelico92@users.noreply.github.com>
2024-03-15 11:57:11 +0000

    terminacion del update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,80 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 const Home = () => {
-  const [tarea, setTareas] = useState([]);
+  const [tarea, setTarea] = useState([]);
   const [text, setText] = useState('');
+  // const [createUser,setCreateUser] = useState('');
  
 const handleKeyDown = (e) => {
   if (e.key === "Enter" && text.trim() !== "") {
-    setText("");
-    setTareas((prevTodos) =>
+    setTarea((prevTodos) =>
       prevTodos.concat({
-        id: prevTodos.length,
-        text: text
+        label: text,
+        done: false
       })
     );
+    setText("");
   }
 };
 
   const handleDelete = (id) => { 
-    setTareas(prevTodos => prevTodos.filter(todo => todo.id !== id)); //Elimina la tarea al pulsar el boton 
+    setTarea(prevTodos => prevTodos.filter(todo => todo.id !== id)); //Elimina la tarea al pulsar el boton 
     
   };
+
+
+  function createUser(){
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/Carmelico92',{
+      method:'POST',
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch(error => console.log(error))
+  }
+
+  function getUser(){
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/Carmelico92',{
+      method:'GET'
+    })
+    .then((response) => response.json())
+    .then((data) => setTarea(data))
+    .catch(error => console.log(error))
+  }
+  console.log(tarea);
+  
+    
+  useEffect( () =>{
+    // createUser()
+   
+    getUser()
+  
+ },[])
+
+  function upDate(){
+    console.log(tarea);
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/Carmelico92',{
+      method:'PUT',
+      body:JSON.stringify(tarea),
+    headers:{
+      "Content-Type": "application/json"
+    }
+    })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => {console.log(error)})
+  }
+
+
+  useEffect( () =>{
+    if (tarea.length > 0){
+      upDate()  
+    } 
+   
+  },[tarea])
 
   return (
     <><h1 className="text-center">todos</h1>
@@ -33,7 +88,7 @@ const handleKeyDown = (e) => {
       <ul className="bg-white">
         {tarea.length > 0 ?  tarea.map(item => (
           <li className="border-bottom list-group-item d-flex justify-content-between align-items-center container-btnDelete" key={item.id}>
-            {item.text}
+            {item.label}
             <button onClick={() => handleDelete(item.id)} className="btnDelete">🗙</button>
           </li>
         )) : "No hay tareas, añade alguna."}
